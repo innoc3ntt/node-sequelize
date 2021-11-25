f1ec29f70934b942542cf2f118725af6682b95fa
Tim <timothy.tan.93@gmail.com>
2021-11-25 09:27:32 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// Ff the filename passed to require is actually a directory, it will first look for package.json in the directory and load the file referenced in the main property. Otherwise, it will look for an index.js.
+
+const classroomController = require('../controllers').classroom;
+router.get('/api/classroom', classroomController.list);
+router.get('/api/classroom/:id', classroomController.getById);
+router.post('/api/classroom', classroomController.add);
+router.put('/api/classroom/:id', classroomController.update);
+router.delete('/api/classroom/:id', classroomController.delete);
+router.post('/api/classroom/add_with_students', classroomController.addWithStudents);
+
+const studentController = require('../controllers').student;
+router.get('/api/student', studentController.list);
+router.get('/api/student/:id', studentController.getById);
+router.post('/api/student', studentController.add);
+router.put('/api/student/:id', studentController.update);
+router.delete('/api/student/:id', studentController.delete);
+router.post('/api/student/add_course', studentController.addCourse);
+
+const lecturerController = require('../controllers').lecturer;
+router.get('/api/lecturer', lecturerController.list);
+router.get('/api/lecturer/:id', lecturerController.getById);
+router.post('/api/lecturer', lecturerController.add);
+router.put('/api/lecturer/:id', lecturerController.update);
+router.delete('/api/lecturer/:id', lecturerController.delete);
+router.post('/api/lecturer/add_with_course', lecturerController.addWithCourse);
+
+const courseController = require('../controllers').course;
+router.get('/api/course', courseController.list);
+router.get('/api/course/:id', courseController.getById);
+router.post('/api/course', courseController.add);
+router.put('/api/course/:id', courseController.update);
+router.delete('/api/course/:id', courseController.delete);
+
+module.exports = router;
